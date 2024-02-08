55df099d3079804735163da372e7fdd96935aff8
Burbank <jonasmaches@gmail.com>
2024-02-08 18:36:57 +0000

    Added cors feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 const userRoutes =  require("./routes/user.js");
 const shipmentRoutes = require("./routes/shipment.js");
@@ -8,6 +9,14 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 //middleware
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/api", userRoutes);
 app.use("/api", shipmentRoutes);
