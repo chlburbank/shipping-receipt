cd3047df6a1a97103d177e894c06d325be91bdd9
Burbank <jonasmaches@gmail.com>
2024-03-04 12:23:27 +0000

    Minor error in origin CORS

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 9000;
 //middleware
 
 const corsOptions ={
-    origin:'https://fathomless-peak-03656-f018de2233df.herokuapp.com/', 
+    origin:'https://fathomless-peak-03656-f018de2233df.herokuapp.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
